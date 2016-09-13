40ce3359bac9e4a7ef6d7544acea903eede70ac6
Marceau Lacroix <mlacroix.externe@pagesjaunes.fr>
2016-09-13 18:02:19 +0200

    stuff

--- .vimrc
+++ .vimrc
@@ -1,2 +1,11 @@
 colorscheme murphy
 cmap w!! w !sudo tee >/dev/null %
+
+execute pathogen#infect()
+syntax on
+
+filetype plugin on
+filetype plugin indent on
+
+"enable mouse wheel/selection
+set mouse=a
--- bash_stuff.sh
+++ bash_stuff.sh
@@ -3,7 +3,7 @@
 #####################
 dev=~/dev
 proj=$dev/audience-stream
-
+export NO_PROXY=localhost
 #####################
 ###### CONF #########
 #####################
@@ -13,6 +13,18 @@ stty -ixon
 #####################
 ##### STUFF #########
 #####################
+function openTunnel {
+  local tunnelName=$1
+  local localPort=$2
+  local remoteHost=$3
+  local remotePort=$4
+
+  nc -nz 127.0.0.1 $localPort
+  if [ $? -ne 0 ]; then
+    echo "Establishing a tunnel to $remoteHost:$remotePort ($tunnelName) on the local port $localPort..."
+    ssh -NfL $localPort:$remoteHost:$remotePort $(whoami)@bigdatabatch03g.be3.local
+  fi
+}
 function editprofile
 {
   atom $CONF_HOME/bash_stuff.sh ~/.bash_aliases ~/dev/scripts/pj.sh &
@@ -37,9 +49,23 @@ function git_force_reset
   fi
 }
 
+function git_grep
+{
+  # Liste des repertoires sans leurs sous-repertoires
+  repos="$(find * -type d -prune)"
+
+  # Pour chaque repo, recherche dans les log la chaine demandée
+  for repo in ${repos}; do
+    cd ${repo}
+    echo ${repo}
+    git leditp  og --grep=$1
+    cd ..
+  done
+}
+
 function gss
 {
-  branch_name=`git rev-parse --abbrev-ref HEAD`
+  branch_name=$(git rev-parse --abbrev-ref HEAD)
   git stash save "$branch_name - working"
 }
 
@@ -64,8 +90,24 @@ function lycos
 
 function merguez
 {
-  ps aux | grep $1
+  ps -edf | grep $1
 }
+
+
+
+# Eternal bash history.
+# ---------------------
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+export HISTTIMEFORMAT="[%F %T] "
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 
 
 export PS1='\[$(tput bold)\]\[\033[38;5;202m\]\u\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] [\[$(tput sgr0)\]\[\033[38;5;211m\]${newPWD}\[\033[38;5;15m\]]$(__git_ps1 " \[\033[1;34m\]{%s}")\[$(tput sgr0)\]\[\033[38;5;15m\]\[$(tput sgr0)\]\[\033[38;5;10m\]>> \[\033[38;5;15m\]'
@@ -74,7 +116,7 @@ export PS1='\[$(tput bold)\]\[\033[38;5;202m\]\u\[$(tput sgr0)\]\[$(tput sgr0)\]
 ###### ALIAS ########
 #####################
 alias vi='vim'
-alias w='thunar .'
+alias w='thunar . &'
 alias b='cd -'
 alias fuuuu='sudo $(history -p \!\!)'
 alias fuu='fuuuu'
